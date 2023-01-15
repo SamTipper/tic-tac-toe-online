7778a14dbbc97778fb2577eceb1d69fad10f1a07
SamTipper <samtipperprogrammer@gmail.com>
2023-01-15 16:35:51 +0000

    Added player styling and messages

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -15,6 +15,7 @@ import { SocketioService } from 'src/app/services/socketio.service';
 export class GameComponent implements OnInit, OnDestroy {
   playerName: string;
   opponentName: string;
+  playerNumber: number;
   players: Object = {};
   nameForm: FormGroup;
   gameCode: number;
@@ -48,14 +49,17 @@ export class GameComponent implements OnInit, OnDestroy {
     if (this.player.playerNumber === undefined
       && localStorage.getItem("rejoinCode") === undefined){ // Join from link with rejoin code
       this.player.playerNumber = 2;
+      this.playerNumber = 2;
 
     } else if (this.player.playerNumber !== undefined 
       && localStorage.getItem("rejoinCode") !== undefined){ // Join from game creation
       this.player.playerNumber = 1;
+      this.playerNumber = 1;
 
     } else if (this.player.playerNumber == undefined 
       && localStorage.getItem("rejoinCode") !== undefined){ // Join from link
       this.player.playerNumber = 2;
+      this.playerNumber = 2;
       
     } 
 
